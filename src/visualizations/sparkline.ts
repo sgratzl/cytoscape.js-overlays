dbc51ffa2805efcb0c3d339890057083a5f30d19
Samuel Gratzl <sam@sgratzl.com>
2020-12-08 22:46:38 -0500

    build: upgrade build system

--- src/visualizations/sparkline.ts
+++ src/visualizations/sparkline.ts
@@ -48,7 +48,7 @@ export function renderSparkLine(
     const xScale = (i: number) => i * step + o.padding;
     const yScale = (v: number) => (1 - yScale01(v)) * dim.height;
 
-    const values = value.map((y, x) => ({ x, y }));
+    const values = value.map((y, x) => ({ x, y: y! }));
 
     const bg = backgroundColor(node);
     if (bg) {
